470b79f2ac0523d0b5637d91d872607451851fda
Charles Pisciotta <cpisciottadeveloping@gmail.com>
2020-05-09 13:32:35 -0400

    Initial Commit

---
+++ FirebaseKit/FirebaseKit.h
@@ -0,0 +1,19 @@
+//
+//  FirebaseKit.h
+//  FirebaseKit
+//
+//  Created by Charles Pisciotta on 5/9/20.
+//  Copyright © 2020 Charles Pisciotta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FirebaseKit.
+FOUNDATION_EXPORT double FirebaseKitVersionNumber;
+
+//! Project version string for FirebaseKit.
+FOUNDATION_EXPORT const unsigned char FirebaseKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirebaseKit/PublicHeader.h>
+
+
