fe6d32bcc9a13b31796570ad7fbdc44abf720d13
Charles Pisciotta <cpisciottadeveloping@gmail.com>
2022-09-03 11:07:24 -0400

    Create FirebaseManager.swift

---
+++ Sources/FirestoreKit/Service/FirebaseManager.swift
@@ -0,0 +1,14 @@
+//
+//  FirebaseManager.swift
+//  
+//
+//  Created by Charles Pisciotta on 9/3/22.
+//
+
+import Foundation
+
+public struct FirebaseManager {
+    public let router = FirestoreRouter()
+
+    public init() {}
+}
