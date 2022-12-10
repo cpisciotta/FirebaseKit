a32346e26c6e5b91b1fbc4cd6922d44a31d9c636
Charles Pisciotta <cpisciottadeveloping@gmail.com>
2022-12-10 16:40:09 -0500

    Replace FirestoreRouter with FirestoreManager

--- Sources/FirestoreKit/Service/FirestoreManager.swift
+++ Sources/FirestoreKit/Service/FirestoreManager.swift
@@ -5,10 +5,72 @@
 //  Created by Charles Pisciotta on 9/3/22.
 //
 
+import FirebaseFirestore
 import Foundation
 
 public struct FirestoreManager {
-    public let router = FirestoreRouter()
+    private let firestore = Firestore.firestore()
 
-    public init() {}
+    public static let shared = FirestoreManager()
+
+    private init() { }
+
+    // MARK: - GET
+
+    @discardableResult
+    public func getDocument(
+        in collection: FirestoreCollection,
+        named documentName: String
+    ) async throws -> DocumentSnapshot {
+        try await firestore
+            .collection(collection.name)
+            .document(documentName)
+            .getDocument()
+    }
+
+    @discardableResult
+    public func getAllDocuments(
+        in collection: FirestoreCollection
+    ) async throws -> QuerySnapshot {
+        try await firestore
+            .collection(collection.name)
+            .getDocuments()
+    }
+
+    // MARK: - CREATE
+
+    @discardableResult
+    public func createDocument(
+        in collection: FirestoreCollection,
+        withData documentData: [String: Any]
+    ) -> DocumentReference {
+        firestore
+            .collection(collection.name)
+            .addDocument(data: documentData)
+    }
+
+    // MARK: - UPDATE
+
+    public func updateDocument(
+        in collection: FirestoreCollection,
+        named documentName: String,
+        fields: [AnyHashable: Any]
+    ) async throws {
+        try await firestore
+            .collection(collection.name)
+            .document(documentName)
+            .updateData(fields)
+    }
+
+    // MARK: - DELETE
+
+    public func deleteDocument(
+        in collection: FirestoreCollection,
+        named documentName: String
+    ) async throws {
+        try await firestore
+            .collection(collection.name)
+            .document(documentName)
+            .delete()
+    }
 }
